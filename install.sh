d0366235ddd35f783e4890e2e8d5acd56c59f427
Piedade <pedro.piedade@red-agency.pt>
2025-05-25 22:22:37 +0100

    fix flameshot folder

--- install.sh
+++ install.sh
@@ -64,14 +64,15 @@ installDependencies() {
     "${SUDO_CMD}" apt upgrade -y
 
     # Installing Essential Programs
-
-
-
     "${SUDO_CMD}" apt-get install build-essential libxcb-util-dev numlockx feh rofi unzip wget pipewire pipewire-audio-client-libraries wireplumber pipewire-pulse pipewire-alsa pavucontrol libx11-dev libxft-dev libxinerama-dev libx11-xcb-dev libxcb-res0-dev alsa-utils xdg-utils libimlib2-dev pkexec lxpolkit gnome-keyring libsecret-1-0 gvfs gvfs-backends gvfs-fuse thunar tumbler-plugins-extra xarchiver thunar-archive-plugin dunst arandr -y
 
     # Installing Other less important Programs
     "${SUDO_CMD}" apt-get install xbindkeys xdotool fzf jq libnotify-bin trash-cli flameshot psmisc neovim papirus-icon-theme lxappearance lightdm xclip bat multitail tree zoxide bash-completion ripgrep alacritty gimp inkscape gimp libreoffice fonts-liberation fonts-noto-color-emoji -y
 
+    # For flameshot screenshots
+    mkdir -p "$USER_HOME/Pictures"
+    # fix permissions
+    "${SUDO_CMD}" chown -R piedade:piedade "$USER_HOME/Pictures"
 
     # Enable graphical login and change target from CLI to GUI
     systemctl enable lightdm
