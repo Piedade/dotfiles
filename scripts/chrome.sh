a120804b37413dbd09e04407a937e3fb016d4218
Pedro Piedade <1780571+Piedade@users.noreply.github.com>
2026-01-25 11:50:40 +0000

    Update Chrome installation script for automatic confirmation

--- scripts/chrome.sh
+++ scripts/chrome.sh
@@ -4,7 +4,7 @@ echo_info "Installing Chrome..."
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 "${SUDO_CMD}" dpkg -i ./google-chrome-stable_current_amd64.deb
-"${SUDO_CMD}" apt-get -f install
+"${SUDO_CMD}" apt-get -f -y install
 rm ./google-chrome-stable_current_amd64.deb
 
 echo_success "Chrome installed!"
