9b7a63d61101968d858e197d00cbe9615cb4ffbe
Pedro Piedade <1780571+Piedade@users.noreply.github.com>
2026-01-25 11:37:34 +0000

    Update hyprtoolkit version to v0.5.3

--- scripts/hyprland/hyprtoolkit.sh
+++ scripts/hyprland/hyprtoolkit.sh
@@ -2,7 +2,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $SCRIPT_DIR/../utils.sh
 
-tag="v0.4.1"
+tag="v0.5.3"
 name="hyprtoolkit"
 
 echo_info "Installing $name $tag..."
