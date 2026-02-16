da3ea4cbee42171b0c5abf13dcdff752c048371e
Pedro Piedade <1780571+Piedade@users.noreply.github.com>
2026-02-16 11:10:03 +0000

    Add installation script for Google Chrome

--- scripts/chrome.sh
+++ scripts/chrome.sh
@@ -2,6 +2,8 @@
 
 echo_info "Installing Chrome..."
 
+"${SUDO_CMD}" apt-get install -y fonts-liberation
+
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 "${SUDO_CMD}" dpkg -i ./google-chrome-stable_current_amd64.deb
 "${SUDO_CMD}" apt-get -f -y install
