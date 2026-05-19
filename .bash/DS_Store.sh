f837bd4ae1183b3e4853d425b66e7f628d90e2c0
Piedade <pedro.piedade@red-agency.pt>
2026-05-19 16:11:25 +0100

    delete also ._.DS_Store

--- .bash/DS_Store.sh
+++ .bash/DS_Store.sh
@@ -1,19 +1,11 @@
 
 delete_DS_Store(){
-    # if [ -z "$1" ]; then
-    #     find . -name '.DS_Store' -delete
-    # else
-    #     find "$1" -name '.DS_Store' -delete
-    # fi
-    if [ -z "$1" ]; then
-        find . -name '.DS_Store' -print0 | while IFS= read -r -d $'\0' file; do
-            echo_info "Deleting: $file"
-            rm "$file"
-        done
-    else
-        find "$1" -name '.DS_Store' -print0 | while IFS= read -r -d $'\0' file; do
-            echo_info "Deleting: $file"
-            rm "$file"
-        done
-    fi
+    # Define o diretório alvo: se passar argumento usa-o, caso contrário usa o diretório atual (.)
+    TARGET_DIR="${1:-.}"
+
+    # O uso de \( -name '.DS_Store' -o -name '._*' \) serve como um "OU" (OR) no find
+    find "$TARGET_DIR" -type f \( -name '.DS_Store' -o -name '._.DS_Store' \) -print0 | while IFS= read -r -d $'\0' file; do
+        echo_info "Deleting: $file"
+        rm "$file"
+    done
 }
