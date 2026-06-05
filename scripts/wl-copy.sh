60662a776a5479a2a72defaa6863baf67af392ce
Piedade <pedro.piedade@red-agency.pt>
2026-06-05 10:52:48 +0100

    fix cliphist

--- scripts/wl-copy.sh
+++ scripts/wl-copy.sh
@@ -3,12 +3,12 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SCRIPT_DIR/check_env.sh"
 
-echo_info "Installing wl-copy..."
+echo_info "Installing cliphist..."
 
-if command_exists wl-copy; then
-    echo_success "wl-clipboard already installed!"
+if command_exists cliphist; then
+    echo_success "cliphist already installed!"
     return
 fi
 
 # Install
-sudo apt-get install -y wl-clipboard cliphist
+sudo apt-get install -y wl-clipboard wl-copy cliphist
