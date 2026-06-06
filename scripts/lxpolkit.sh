55e19213fb25414edf8000bcf742bb31f7d335c7
Piedade <pedro.piedade@red-agency.pt>
2026-06-06 22:48:04 +0100

    add pkexec

--- scripts/lxpolkit.sh
+++ scripts/lxpolkit.sh
@@ -7,9 +7,9 @@ echo_info "Installing Polkit Agent..."
 
 if command_exists lxpolkit; then
     echo_success "Polkit Agent already installed!"
-    return
+    return 2>/dev/null || exit 0
 fi
 
-sudo apt-get install -y polkitd lxpolkit gnome-keyring # libsecret-1-0
+sudo apt-get install -y polkitd lxpolkit gnome-keyring pkexec # libsecret-1-0
 
 echo_success "Polkit installed!"
