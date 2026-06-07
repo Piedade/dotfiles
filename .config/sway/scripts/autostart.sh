f09b9a2e24f8d5baaaf54a8350453960a9b99dfb
Piedade <pedro.piedade@red-agency.pt>
2026-06-07 11:20:53 +0100

    save ssh key

--- .config/sway/scripts/autostart.sh
+++ .config/sway/scripts/autostart.sh
@@ -17,6 +17,7 @@ lxpolkit &
 eval $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
 export SSH_AUTH_SOCK
 systemctl --user import-environment SSH_AUTH_SOCK
+ssh-add ~/.ssh/id_ed25519 2>/dev/null
 
 ## Clipboard history watcher
 wl-paste --watch cliphist store &
